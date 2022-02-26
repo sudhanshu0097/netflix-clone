6a392df8a5658cd2bfd4d7f1bd66c1a707a77b3d
sudhanshu0097 <sudhanshu0097@gmail.com>
2022-02-26 20:11:19 +0530

    added movie

--- src/Movie.js
+++ src/Movie.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import Banner from './Banner'
+import Nav from './Nav'
 
 function Movie() {
   return (
-    <div>
+    <div className='movie'>
+
+      <Nav/>
+      <Banner/>
       
     </div>
   )
