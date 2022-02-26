7fffa798c1d1bb6f44eb1fc7eee385ffe5ba3be8
sudhanshu0097 <sudhanshu0097@gmail.com>
2022-02-27 00:48:53 +0530

    post

--- src/Profile.js
+++ src/Profile.js
@@ -47,7 +47,39 @@ function Profile() {
             <div className="profile_plans">
               <h3>Member since :- {user.created[1]+"/"+user.created[2] + "/" +user.created[3]}</h3>
               <h3>My Available Plans</h3>
-                
+              <div className="plan_details">
+                <div className="plan_details_child">
+                  <h3>Basic</h3>
+                  <h3>Price-:200</h3>
+                  
+                  </div>  
+
+                  <div className="plan_details_child">
+                  <h3>Standard</h3>
+                  <h3>Price-:400</h3>
+                  
+                  </div> 
+
+                  <div className="plan_details_child">
+                  <h3>H.D(1080px)</h3>
+                  <h3>Price-:600</h3>
+                  
+                  </div> 
+
+                  <div className="plan_details_child">
+                  <h3>4k(2160px)</h3>
+                  <h3>Price-:800</h3>
+                  
+                  </div> 
+
+                  <div className="plan_details_child">
+                  <h3>Mobile version</h3>
+                  <h3>Price-:149</h3>
+                  
+                  </div> 
+
+              </div>
+
               <button onClick={()=>{
                   auth.signOut();
                   navigate("/")
