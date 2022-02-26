fb73157afba5ec4f0841e9f834d9c4edc3eb0e6b
sudhanshu0097 <sudhanshu0097@gmail.com>
2022-02-26 20:29:57 +0530

    added movie

--- src/Movie.js
+++ src/Movie.js
@@ -11,7 +11,7 @@ function Movie() {
       <Nav/>
       <Banner/>
 
-      <Row title="Adventure Movies" fetchUrl={requests.fetchMovieAdventure} ></Row>
+      <Row title="Adventure Movies" fetchUrl={requests.fetchMovieAdventure} isLargeRow ></Row>
 
       <Row title="Crime Movies" fetchUrl={requests.fetchMovieCrime} ></Row>
 
@@ -22,6 +22,8 @@ function Movie() {
       <Row title="History Movies" fetchUrl={requests.fetchMovieHistory} ></Row>
 
       <Row title="Horror Movies" fetchUrl={requests.fetchMovieHorror} ></Row>
+
+      <Row title="Science-Fiction" fetchUrl={requests.fetchMovieSci} ></Row>
 
       
 
